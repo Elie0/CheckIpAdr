22f357d369dc916993c7143c6afb128f1b427043
Elie Aro <eliearo123@gmail.com>
2023-02-01 14:01:58 +0200

    comments

--- functions.js
+++ functions.js
@@ -1,5 +1,6 @@
 
-const dec2bin =(dec)=>{
+const dec2bin =(dec)=>{            // I additionaly added this fct in order to convert the input ip adress to binary form. This fct
+                               // will be calledd in getBinary fct later
 
     let ans = (dec >>> 0).toString(2);
     while(ans.length < 8) {
@@ -10,26 +11,26 @@ const dec2bin =(dec)=>{
 }
 
 
-const checkIpAdress = (ip) =>{
+const checkIpAdress = (ip) =>{        // mainly this is the core fct of the code
 
-const separated = ip.split('.')
+const separated = ip.split('.') // here, I get the ip written like 192.168.0.1 and split it by the . it returns an array [192,168,0,1]
 
-if(separated.length !==4)
+if(separated.length !==4) // if length is not 4, then the ip is of course not valid like for example we can't have 192.168 
 {
     return ('ip Adress is not valid!')
 }
 
-for(let i=0;i<separated.length;i++) {
+for(let i=0;i<separated.length;i++) { // here I loop and access all the parts in the array[192,68,0,1]
 
-    if( !(Number.isInteger(Number(separated[i]))===true))
+    if( !(Number.isInteger(Number(separated[i]))===true))  // this is used to ensure that no string is written like 19a.b25
         {
            return('The input should only be integer')
         }
-    if(separated[i].length>1 && separated[i][0]===0)
+    if(separated[i].length>1 && separated[i][0]===0) // this is used to ensure that we cannot start like 091.292.04.3
         {
             return('cannot start numbers with leading 0')
         }
-    if(!(separated[i]>=0 && separated[i]<=255))
+    if(!(separated[i]>=0 && separated[i]<=255))  // this to ensure that the max range resides between 0-255
     {
         return('ip adress is outside of range!!')
     }
@@ -39,16 +40,16 @@ return true
 
 }
 
-const getClass = (ip)=>{
-    const classIndicator = ip.split('.')
-    if(classIndicator[0] >=192 && classIndicator[0] <=223)
+const getClass = (ip)=>{   // this fct is used to determine the ip class
+    const classIndicator = ip.split('.')  // same as before
+    if(classIndicator[0] >=192 && classIndicator[0] <=223)   // class C
     {
          const send = 'Class C'
          const networkMask = classIndicator[0]+'.'+classIndicator[1]+'.'+classIndicator[2]+'.0'
-         return [send,`Network Mask: ${networkMask}`,'Default Subnet: 255.255.255.0']          
+         return [send,`Network Mask: ${networkMask}`,'Default Subnet: 255.255.255.0']  // this array is being used to display data as I want on the frontend        
          
     }
-    else if(classIndicator[0] >=128 && classIndicator[0] <=191)
+    else if(classIndicator[0] >=128 && classIndicator[0] <=191)  // same logic 
     {
         const send = 'Class B'
          const networkMask = classIndicator[0]+'.'+classIndicator[1]+'.0'+'.0'
@@ -56,28 +57,28 @@ const getClass = (ip)=>{
     }
     else
     {
-        const send = 'Class A'
+        const send = 'Class A'    // same logic
         const networkMask = classIndicator[0]+'.0'+'.0'+'.0'
         return [send,`Network Mask: ${networkMask}`,'Default Subnet: 255.0.0.0'] 
     }
    
 }
 
-const getBinary =(ip)=>{
-    const IP = ip.split('.')
+const getBinary =(ip)=>{            // finally this fct transforms the ip inpput into binary
+    const IP = ip.split('.') // getting the Ip array
     let binIp =''
     let count=0
     IP.forEach(element => {
         count++
-        const bin =dec2bin(Number(element))
+        const bin =dec2bin(Number(element)) // I used my fct defined before
         if(count===4)
         {
-            binIp= binIp.concat(bin)
+            binIp= binIp.concat(bin) 
         }
         else
-        binIp= binIp.concat(bin).concat('.')
+        binIp= binIp.concat(bin).concat('.') // while count is not 4, I concatenate the '.'
     });
-     return ('BinaryForm:')+binIp
+     return ('BinaryForm:'+binIp)
 }
 
 
--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const {checkIpAdress,getClass,getBinary}= require('./functions')
+const {checkIpAdress,getClass,getBinary}= require('./functions')         // These are all the functions used to provide the backend of the app
 const http = require('http')
-const express = require('express')
+const express = require('express')     // all of these are used to provide a front end interface, 
 const socketio = require('socket.io')
 const app = express()
 app.use(express.static('public'))
-const server = http.createServer(app)   // to use socket.io
+const server = http.createServer(app)   
 const io = socketio(server)
 
 const port = process.env.PORT || 3000
 
 
-io.on('connection',(socket)=>{       // we can use io to communicate with a specific client if i have diff clients it will run many times
+io.on('connection',(socket)=>{    // you dont need to read all of this, just go to function.js where the logic of ip checker resides
     console.log('New Websocket connection')
-socket.on('checkip',(ip)=>{
-    if(checkIpAdress(ip)===true){
+socket.on('checkip',(ip)=>{        // by socket.on I read the user input from the form. You can if you want check how the communication was done in the main.js file
+    if(checkIpAdress(ip)===true){           // here I call checkIpadress, if it is true, I emit to the frontend the data calculation below
         socket.emit('result','Valid ip')
         socket.emit('toBinary',getBinary(ip))
         socket.emit('classInfo',getClass(ip))
     }
     else{
-        socket.emit('result',checkIpAdress(ip))
+        socket.emit('result',checkIpAdress(ip))  // else I only return the result, which will eventally be describing why ip adress i not valid
     }
     
 })
